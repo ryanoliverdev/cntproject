754cb532606699983d55aac5826a5f03b34d5d8e
ryanoliverdev <ryanoliverdev@gmail.com>
2023-10-16 22:50:35 -0400

    Added in reading the common.cfg and peerinfo.cfg files. Begun working on setting up peers

--- src/Server.java
+++ src/Server.java
@@ -1,55 +1,98 @@
 import java.net.*;
 import java.io.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.util.*;
 
 public class Server {
-    private Socket clientSocket;
-    private ServerSocket serverSocket;
-    private PrintWriter output;
-    private BufferedReader input;
 
-    public void startServer(int port)
-    {
-        try
-        {
-            serverSocket = new ServerSocket(port);
-            clientSocket = serverSocket.accept();
+    private static final int sPort = 8000;   //The server will be listening on this port number
 
-            output = new PrintWriter(clientSocket.getOutputStream(), true);
-            input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-            String test = input.readLine();
-
-            if("hello jonathan".equals(test))
-            {
-                output.println("hello friendly client");
-            }
-            else
-            {
-                output.println("Greeting not working");
-            }
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void stop() {
+    public static void main(String[] args) throws Exception {
+        System.out.println("The server is running.");
+        ServerSocket listener = new ServerSocket(sPort);
+        int clientNum = 1;
         try {
-            input.close();
-            output.close();
-            clientSocket.close();
-            serverSocket.close();
+            while(true) {
+                new Handler(listener.accept(),clientNum).start();
+                System.out.println("Client "  + clientNum + " is connected!");
+                clientNum++;
+            }
+        } finally {
+            listener.close();
         }
-        catch (IOException error)
-        {
-            throw new RuntimeException(error);
-        }
+
     }
 
-    public static void main(String[] args) {
-        Server server = new Server();
-        server.startServer(6666);
+    /**
+     * A handler thread class.  Handlers are spawned from the listening
+     * loop and are responsible for dealing with a single client's requests.
+     */
+    private static class Handler extends Thread {
+        private String message;    //message received from the client
+        private String MESSAGE;    //uppercase message send to the client
+        private Socket connection;
+        private ObjectInputStream in;	//stream read from the socket
+        private ObjectOutputStream out;    //stream write to the socket
+        private int no;		//The index number of the client
+
+        public Handler(Socket connection, int no) {
+            this.connection = connection;
+            this.no = no;
+        }
+
+        public void run() {
+            try{
+                //initialize Input and Output streams
+                out = new ObjectOutputStream(connection.getOutputStream());
+                out.flush();
+                in = new ObjectInputStream(connection.getInputStream());
+                try{
+                    while(true)
+                    {
+                        //receive the message sent from the client
+                        message = (String)in.readObject();
+                        //show the message to the user
+                        System.out.println("Receive message: " + message + " from client " + no);
+                        //Capitalize all letters in the message
+                        MESSAGE = message.toUpperCase();
+                        //send MESSAGE back to the client
+                        sendMessage(MESSAGE);
+                    }
+                }
+                catch(ClassNotFoundException classnot){
+                    System.err.println("Data received in unknown format");
+                }
+            }
+            catch(IOException ioException){
+                System.out.println("Disconnect with Client " + no);
+            }
+            finally{
+                //Close connections
+                try{
+                    in.close();
+                    out.close();
+                    connection.close();
+                }
+                catch(IOException ioException){
+                    System.out.println("Disconnect with Client " + no);
+                }
+            }
+        }
+
+        //send a message to the output stream
+        public void sendMessage(String msg)
+        {
+            try{
+                out.writeObject(msg);
+                out.flush();
+                System.out.println("Send message: " + msg + " to Client " + no);
+            }
+            catch(IOException ioException){
+                ioException.printStackTrace();
+            }
+        }
+
     }
 
 }
